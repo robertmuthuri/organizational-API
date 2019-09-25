c54bec6f6647d7b321e7655f7f6a1c474f05de69
Robert Muthuri <muthuri.r@gmail.com>
2019-09-25 15:21:44 +0300

    Adds setters and override method to news class.

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.Objects;
+
 public class News {
     public int id;
     public String heading;
@@ -17,4 +19,23 @@ public class News {
     public String getHeading() { return heading; }
     public String getNews_link() { return news_link; }
     public String getNews_type() { return news_type; }
+
+    public void setId(int id) { this.id = id; }
+    public void setHeading(String heading) { this.heading = heading; }
+    public void setNews_link(String news_link) { this.news_link = news_link; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        News news = (News) o;
+        return getId() == news.getId() &&
+                Objects.equals(getHeading(), news.getHeading()) &&
+                Objects.equals(getNews_link(), news.getNews_link()) &&
+                Objects.equals(getNews_type(), news.getNews_type());
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getHeading(), getNews_link(), getNews_type());
+    }
 }
