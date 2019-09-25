08a48ec054b2ff35fccea2eb2bba8bd5e65bec39
Robert Muthuri <muthuri.r@gmail.com>
2019-09-25 15:17:52 +0300

    Adds setters and override methods to department class.

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,23 +1,47 @@
 package models;
 
+import java.util.Objects;
+
 public class Department {
 
     private int id;
+    private String name;
     private String description;
     private int total_employees;
-    private String department_type;
     private int user_id;
 
-    public Department( String description, int total_employees, String news_link, String department_type,
+    public Department(String name, String description, int total_employees, String news_link,
                  int user_id) {
+        this.name = name;
         this.description = description;
         this.total_employees = total_employees;
-        this.department_type = department_type;
         this.user_id = user_id;
     }
     public int getId() { return id; }
-    public String getFull_name() { return description; }
-    public int getPosition() { return total_employees; }
-    public String getDepartment() { return department_type; }
+    public String getDepartment() { return name; }
+    public String getDescription() { return description; }
+    public int getTotal_employees() { return total_employees; }
     public int getUser_post() { return user_id; }
+
+    public void setId(int id) { this.id = id; }
+    public void setDepartment_type(String name) { this.name = name; }
+    public void setDescription(String description) { this.description = description; }
+    public void setTotal_employees(int total_employees) { this.total_employees = total_employees; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return getId() == that.getId() &&
+                getTotal_employees() == that.getTotal_employees() &&
+                user_id == that.user_id &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(getDescription(), that.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), name, getDescription(), getTotal_employees(), user_id);
+    }
 }
